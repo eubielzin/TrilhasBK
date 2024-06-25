74ada0ed682d011824cba112f37c2c0209ede259
najuliag <ajgalmeida12345@gmail.com>
2024-06-24 21:07:05 -0300

    feat: adicionando o metodo findById

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -9,6 +9,20 @@ userRouter.get('/user', async (request , response) => {
     return response.send({ user })
 })
 
+userRouter.get('/user/:id', async (request, response) => {
+    const { id } = request.params
+
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(id, 10) }
+    })
+
+    if (!user) {
+        return response.status(404).send({ message: 'User not found' })
+    }
+
+    return response.send({ user })
+})
+
 userRouter.post('/user', async(request, response) => {
     const { nome, email, Senha, tipo_usuario } = request.body
 
